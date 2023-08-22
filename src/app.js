5eadd7ab24325d655fad89aee60cbefaeb5e7907
Maryna007-hub <marinagolinka007@gmail.com>
2023-08-22 19:09:59 +0100

    Create function search and move let to function

--- src/app.js
+++ src/app.js
@@ -40,16 +40,19 @@ function displayTemperature(response) {
     iconElement.setAttribute('src', `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute('alt', response.data.weather[0].description);
 }
+function search(city) {
+    let apiKey = "ed7bf7f5cf99619f0aa2717501c76f85";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+      axios.get(apiUrl).then(displayTemperature);    
+}
+
 function handleSubmit(event) {
     event.preventDefault();
     let cityInputElement = document.querySelector('#city-input');
     console.log(cityInputElement.value);
 }
 
-let apiKey = "ed7bf7f5cf99619f0aa2717501c76f85";
-let city = 'Oslo';
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayTemperature);
+
 
   let form = document.querySelector('#search-form');
   form.addEventListener('submit', handleSubmit);
