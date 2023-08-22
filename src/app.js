e53211db001ff27fde989c74d120b7698a0a15f7
Maryna007-hub <marinagolinka007@gmail.com>
2023-08-22 17:25:53 +0100

    Change alt to current description

--- src/app.js
+++ src/app.js
@@ -40,6 +40,7 @@ function displayTemperature(response) {
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute('src', `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute('alt', response.data.weather[0].description);
 }
 
 let apiKey = "ed7bf7f5cf99619f0aa2717501c76f85";
