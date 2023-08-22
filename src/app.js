6b23e9a46782c2d564254dadc8fbb51580c4620d
Maryna007-hub <marinagolinka007@gmail.com>
2023-08-22 20:19:34 +0100

    delete alert and add let

--- src/app.js
+++ src/app.js
@@ -31,6 +31,8 @@ function displayTemperature(response) {
     let dateElement = document.querySelector('#date');
     let iconElement = document.querySelector('#icon');
 
+    celsiusTemperature = response.data.main.temp;
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
@@ -53,18 +55,19 @@ function handleSubmit(event) {
 }
 function displayFahrenheitTemperature(event) {
     event.preventDefault();
-    alert('Link clicked');
-    let fahrenheitTemperature = (temperatureElement.innerHTML * 9 / 5) + 32
-    alert(fahrenheitTemperature);
+   
+    let fahrenheitTemperature = (celsiusTemperature * 9 / 5) + 32
+   
     let temperatureElement = document.querySelector('#temperature');
     temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
+let celsiusTemperature = null;
 
-search('Lisbon');
   let form = document.querySelector('#search-form');
   form.addEventListener('submit', handleSubmit);
 
   let fahrenheitLink = document.querySelector('#fahrenheit-link');
   fahrenheitLink,addEventListener('click', displayFahrenheitTemperature);
 
+  search('Lisbon');
 
