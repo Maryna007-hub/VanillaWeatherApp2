7a5f808d0dd5e27a21e0fa4f5fea1f618eabf0e2
Maryna007-hub <marinagolinka007@gmail.com>
2023-08-22 20:50:19 +0100

    Add function C

--- src/app.js
+++ src/app.js
@@ -59,11 +59,11 @@ function displayFahrenheitTemperature(event) {
     let fahrenheitTemperature = (celsiusTemperature * 9 / 5) + 32;
     temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
-// function displayCelsiusTemperature(event) {
-   //  event.preventDefault();
- //    let temperatureElement = document.querySelector('#temperature');
- //    temperatureElement.innerHTML = Math.round(celsiusTemperature);
-// }
+ function displayCelsiusTemperature(event) {
+    event.preventDefault();
+     let temperatureElement = document.querySelector('#temperature');
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+ }
 
 let celsiusTemperature = null;
 
@@ -73,9 +73,8 @@ let celsiusTemperature = null;
   let fahrenheitLink = document.querySelector('#fahrenheit-link');
   fahrenheitLink.addEventListener('click', displayFahrenheitTemperature);
 
-  // let celsiusLink = document.querySelector('#celsius-link');
- 
-  // celsiusLink.addEventListener('click', displayCelsiusTemperature);
+   let celsiusLink = document.querySelector('#celsius-link');
+   celsiusLink.addEventListener('click', displayCelsiusTemperature);
 
   search('Lisbon');
 
