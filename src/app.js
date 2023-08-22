3b7fe943dbc807329d0b6f5ae82a56b66f4502eb
Maryna007-hub <marinagolinka007@gmail.com>
2023-08-22 15:26:29 +0100

    Created function displayTemperature

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
-let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
-  
+function displayTemperature(response) {
+    console.log(response.data);
+}
+
+let apiKey = "ed7bf7f5cf99619f0aa2717501c76f85";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
