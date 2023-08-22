15ce7f99255340f8785871bba0b0ce7391ccbac0
Maryna007-hub <marinagolinka007@gmail.com>
2023-08-22 15:37:10 +0100

    Made round temperature

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 function displayTemperature(response) {
     console.log(response.data);
+    let temperatureElement = document.querySelector('#temperature');
+    temperatureElement.innerHTML =Math.round(response.data.main.temp);
 }
 
 let apiKey = "ed7bf7f5cf99619f0aa2717501c76f85";
