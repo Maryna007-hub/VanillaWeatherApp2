f5b1aa6fba6eb562416a0935fa163b2eb00b565d
Maryna007-hub <marinagolinka007@gmail.com>
2023-08-22 16:15:49 +0100

    Added wind, humidity value

--- src/app.js
+++ src/app.js
@@ -2,11 +2,16 @@ function displayTemperature(response) {
     console.log(response.data);
     let temperatureElement = document.querySelector('#temperature');
     let cityElement = document.querySelector('#city');
-    let descriptionElement = document.querySelector('#description');
+    let descriptionElement = document.querySelector('#description1');
+    let humidityElement = document.querySelector('#humidity');
+    let windElement = document.querySelector('#wind');
+
 
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
-     descriptionElement.innerHTML = response.data.wheather[0].main;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.main.wind.speed);
 }
 
 let apiKey = "ed7bf7f5cf99619f0aa2717501c76f85";
