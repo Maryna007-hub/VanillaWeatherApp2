4349aab55dd99dc2d64403196c001ed0a5170c7b
Maryna007-hub <marinagolinka007@gmail.com>
2023-08-22 15:54:22 +0100

    Added text-transform and descriptionElement

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 function displayTemperature(response) {
     console.log(response.data);
     let temperatureElement = document.querySelector('#temperature');
-    temperatureElement.innerHTML =Math.round(response.data.main.temp);
+    let cityElement = document.querySelector('#city');
+    let descriptionElement = document.querySelector('#description');
+
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+     descriptionElement.innerHTML = response.data.wheather[0].main;
 }
 
 let apiKey = "ed7bf7f5cf99619f0aa2717501c76f85";
